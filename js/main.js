2dd97dd0ef917dc1a99301683a1eaa7b7fa99fea
Antonio Leonti <antoniovleonti@icloud.com>
2022-05-13 16:29:55 -0400

    optimization; cooler colors

--- js/main.js
+++ js/main.js
@@ -28,68 +28,78 @@ const material = new THREE.RawShaderMaterial({
 });
 material.vertexColors = true;
 
-
-let pointsArr = []
-let sourcesArr = []
-let posiAttrArr = []
-let geometryArr = []
+const curPos = new Array(NLINES);
+const startPos = new Array(NLINES);
+const posAttr = new Array(NLINES);
+const curColor = new Array(NLINES);
+const colAttr = new Array(NLINES);
+const distFromCenter = new Array(NLINES);
 for (let i = 0; i < NLINES; i++)
 {
-    const points = [
+    curPos[i] = [
         Math.random()*2-1, Math.random()*2-1, Math.random()*2-1, // random start
         0, 0, 0 // points to center
     ];
-    pointsArr.push(points);
-    sourcesArr.push([points[0],points[1],points[2]]);
 
-    const positionAttribute = new THREE.Float32BufferAttribute(points, 3);
-    posiAttrArr.push(positionAttribute);
+    startPos[i] = [curPos[i][0],curPos[i][1],curPos[i][2]];
+    distFromCenter[i] = 
+        Math.sqrt(  Math.exp(startPos[i][0],2) +
+                    Math.exp(startPos[i][1],2));
 
-    let colors = new Float32Array([
-        (points[0]+1)/2*255, (points[1]+1)/2*255, 255, 255, // color 1
+    posAttr[i] = new THREE.Float32BufferAttribute(curPos[i], 3);
+    posAttr[i].setUsage( THREE.StreamDrawUsage );
+
+    //posAttr[i] = positionAttribute;
+
+    curColor[i] = new Float32Array([
+        (curPos[i][0]+1)/2*255, (curPos[i][1]+1)/2*255, 255, 255, // color 1
         0, 0, 0, 0 // color 2
     ]);
-    const colorAttribute = new THREE.Uint8BufferAttribute( colors, 4 );
-    colorAttribute.normalized = true; // maps attribute to [0..1] in shader
 
+    colAttr[i] = new THREE.Uint8BufferAttribute( curColor[i], 4 );
+    colAttr[i].normalized = true; // maps attribute to [0..1] in shader
 
     const geometry = new THREE.BufferGeometry();
-    geometryArr.push(geometry);
 
-    geometry.setAttribute('position', positionAttribute);
-    geometry.setAttribute('color', colorAttribute);
+    geometry.setAttribute('position', posAttr[i]);
+    geometry.setAttribute('color', colAttr[i]);
 
     const line = new THREE.Line( geometry, material );
     scene.add( line );
 }
 
-// keep track of the mouse position
-const mouse = new THREE.Vector2();
+// track the mouse position
 document.addEventListener('mousemove', () => {
     // 'event' keyword is deprecated but it works :-)
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
+    const x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    const y = -( event.clientY / window.innerHeight ) * 2 + 1;
 
     for(let i = 0; i < NLINES; i++)
     {
-        pointsArr[i][3] = mouse.x;
-        pointsArr[i][4] = mouse.y;
-        posiAttrArr[i].set(pointsArr[i]);
-    }
-}, false);
+        curPos[i][3] = x;
+        curPos[i][4] = y;
 
-const updateLineSources = function(time)
+        // update colors based on mouse pos
+        curColor[i][2] = (1 - Math.min(1,Math.sqrt(
+            Math.pow(x - startPos[i][0],2) +
+            Math.pow(y - startPos[i][1],2)
+        ))) * 255;
+        colAttr[i].set(curColor[i]);
+        colAttr[i].needsUpdate = true;
+    }
+});
+
+const updateLines = function(time)
 {
     for (let i = 0; i < NLINES; i++)
     {
-        const distFromCenter = Math.sqrt(Math.exp(sourcesArr[i][0],2)+Math.exp(sourcesArr[i][1],2));
-        const intensity = (Math.sin(time+distFromCenter*10) + 1)/2;
+        const intensity = 0.01 * (Math.sin(time+distFromCenter[i]*30));
         // offset source based on time and location
-        pointsArr[i][0] = sourcesArr[i][0] + Math.cos(time)*0.01*intensity;
-        pointsArr[i][1] = sourcesArr[i][1] + Math.sin(time)*0.01*intensity;
+        curPos[i][0] = startPos[i][0] + Math.cos(time) * intensity;
+        curPos[i][1] = startPos[i][1] + Math.sin(time) * intensity;
         // update attribute
-        posiAttrArr[i] = new THREE.Float32BufferAttribute( pointsArr[i], 3 );
-        geometryArr[i].setAttribute("position", posiAttrArr[i]);
+        posAttr[i].set(curPos[i]);
+        posAttr[i].needsUpdate = true;
     }
 }
 
@@ -100,7 +110,7 @@ const rendering = function()
     time += 0.1;
     // Rerender every time the page refreshes (pause when on another tab)
     requestAnimationFrame(rendering);
-    updateLineSources(time);
+    updateLines(time);
     renderer.render(scene, camera);
 }
 rendering();
